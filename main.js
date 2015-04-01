3984b8c978b17e348d7cd4bd3e59d87ee1bd731b
Alexey_Kh <alexey.kharkin@gmail.com>
2015-04-01 19:32:14 +0300

    Add comments to js file

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
-var searchDropdown = $('.search').html();
-var navDropdown = $('.sidebar nav').html();
-$('.dropdown-search').append(searchDropdown);
-$('.dropdown-menu').append(navDropdown);
+$(document).ready(function(){
 
-$('#icon-search').click(function() {
-  $('.dropdown-menu').hide();
-  $('.dropdown-search').toggle();
-});
+  /* jQuery events */
 
-$('#icon-menu').click(function() {
-  $('.dropdown-search').hide();
-  $('.dropdown-menu').toggle();
-});
+  // Get html of search and navigation elements
+  var searchDropdown = $('.search').html();
+  var navDropdown = $('.sidebar nav').html();
+  // Insert html of search and navigation elements into dropdowns
+  $('.dropdown-search').append(searchDropdown);
+  $('.dropdown-menu').append(navDropdown);
 
-/* jQuery media queries */
 
-$(window).resize(function() {
-  if ($(window).width() > 767 ) {
-    $('[class*="dropdown"]').hide();
-  }
+  // Events for dropdown buttons
+  $('#icon-search').on('click', function() {
+    $('.dropdown-menu').hide();
+    $('.dropdown-search').toggle();
+  });
+
+  $('#icon-menu').on('click', function() {
+    $('.dropdown-search').hide();
+    $('.dropdown-menu').toggle();
+  });
+
+  /* jQuery media queries */
+
+  // Hide dropdowns on large screens, if they were opened before increasing the width of the window
+  $(window).resize(function() {
+    if ($(window).width() > 767 ) {
+      $('[class*="dropdown"]').hide();
+    }
+  });
+
 });
